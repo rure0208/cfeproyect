e47a9ce9576d9fce60dbd41fa001c6bd85104ebd
Efrain_R_Ruiz <79539402+rure0208@users.noreply.github.com>
2022-11-09 12:51:05 -0800

    fecha e intento rpe
    Se agrego un calendario para mantenimiento y muestra datos del rpe de la base

--- cfeproyect/components/AgregarMantenimiento.tsx
+++ cfeproyect/components/AgregarMantenimiento.tsx
@@ -1,18 +1,32 @@
 import React from 'react'
-import { Grid, TextInput, Space, Button, Container } from '@mantine/core'
+import { Grid, TextInput, Space, Button, Container,Select ,NativeSelect} from '@mantine/core'
 import { FcPlus } from 'react-icons/fc'
-import { useState } from 'react';
+import { useState,useEffect } from 'react';
+import { DatePicker } from '@mantine/dates';
 import axios from 'axios';
+import api from '../services/api';
+
 const AgregarMantenimiento = () => {
+  const [dataw, setDataw] = useState([]);
+  useEffect(() => {
+      init();
+  }, [])
+
+  async function init() {
+      const list = await api.listaDePersonal();
+      setDataw(list.data);
+      
+  }
 
   const [noInventario, setNoInventario] = useState('');
   const [centroCoste, setCentroCoste] = useState('');
   const [noSerie, setNoSerie] = useState('');
   const [rpe, setRpe] = useState('');
-  const [fecha, setFecha] = useState('');
+  const [fecha, setFecha] = useState(new Date());
   const baseURL = "http://localhost:1337/api/mantenimientos";
 
   async function createPost() {
+    
     await axios.post(baseURL, {
       data: {
         noInventario: noInventario,
@@ -54,16 +68,24 @@ const AgregarMantenimiento = () => {
             label="Centro de Coste"
             withAsterisk
             value={centroCoste} onChange={(event) => setCentroCoste(event.currentTarget.value)}
+          />
+        {
+                            dataw.map(d => {
+                                return (
+                                    <Select key={d.id} data={[d.attributes.rpe]}>
+                                       
+                                    </Select>
+                                )
+                            })
+                        }
 
-          />
-          <TextInput
-            label="RPE"
-            value={rpe} onChange={(event) => setRpe(event.currentTarget.value)}
-          />
-          <TextInput
-            label="Fecha"
-            value={fecha} onChange={(event) => setFecha(event.currentTarget.value)}
-          />
+             <DatePicker
+      placeholder="Seleccione una fecha"
+      label="Fecha"
+      inputFormat="DD/MM/YYYY"
+      value={fecha}
+          
+    />
         </Grid.Col>
 
         <Grid.Col span={4}>
