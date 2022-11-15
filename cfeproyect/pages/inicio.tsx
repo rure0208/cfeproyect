1287c90e060a9309637ec03c91eebe0618097251
KarenMeza <al18760083@ite.edu.mx>
2022-11-15 12:55:21 -0800

    Diseño Mantenimiento del dia

--- cfeproyect/pages/inicio.tsx
+++ cfeproyect/pages/inicio.tsx
@@ -1,23 +1,37 @@
 import React from 'react';
 import AppShel from '../components/appshel';
 import Layout from '../components/Layout';
-import { Container, Space} from '@mantine/core';
+import { Container, Space,Card,Text} from '@mantine/core';
 import TablaMantenimiento from '../components/TablaMantenimiento';
+import { VscSettingsGear } from 'react-icons/vsc'
 
 const Inicio = () => {
 
   return (
     <Layout tituloPestaña='Inicio'>
       <AppShel tituloPagina='Mantenimiento del día'>
-      <Container size={900} px={0} >
-      <hr/>
-      <Space h="lg" />
-      <Space h="lg" />
-      <Space h="lg" />
-        <div>
-          <TablaMantenimiento />
-        </div>
-      
+      <Container size={700} px={0} >
+
+      <Card style={{ 
+                background: '#758E75',
+                // height: 700,
+                width: 700,
+                color: 'white' 
+            }}>
+            <Text><VscSettingsGear/>   Mantenimiento del dia</Text>
+            <hr/>
+
+            <Space h="lg" />
+            <Card.Section style={{ 
+                background: '#D9D9D9',
+                 height: 400,
+                width: 600, 
+                marginLeft: 40,
+                marginBottom: 4
+            }}>
+              <TablaMantenimiento/>
+            </Card.Section>
+            </Card>
         </Container>
       </AppShel>
     </Layout>
